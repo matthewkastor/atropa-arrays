b2333e54993f67a86f12cf50299d2531faf2b0e1
Matthew Kastor <matthewkastor@gmail.com>
2013-09-29 01:12:59 -0400

    regenerated from source.

--- docs/vsdoc/symbols/atropa-vsdoc.js
+++ docs/vsdoc/symbols/atropa-vsdoc.js
@@ -6,6 +6,9 @@
 
     window.atropa = {
         /// <summary></summary>
+        /// <field name="inquire" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-inquire/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
         /// <field name="arrays" type="">Utilities for handling arrays.</field>
         /// <returns type="atropa"/>
                 
