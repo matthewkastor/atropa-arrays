76c26b521939120ce100124f3add910721b368d4
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 13:20:43 -0500

    updated docs

--- docs/vsdoc/OpenLayersReferences.js
+++ docs/vsdoc/OpenLayersReferences.js
@@ -1,4 +1,6 @@
 
 /// <reference path="atropa-vsdoc.js" />
 /// <reference path="atropa.arrays-vsdoc.js" />
+/// <reference path="atropa.data-vsdoc.js" />
+/// <reference path="atropa.inquire-vsdoc.js" />
 
--- src/atropa-arrays.js
+++ src/atropa-arrays.js
@@ -6,14 +6,6 @@
  * @namespace Container for all Glorious classes, functions, etc.
  */
 var atropa = require('atropa-header');
-/**
- * Required module, the docs for it are in the <code>
- *  atropa-inquire/docs</code> directory where this module 
- *  is located.
- * @see <a href="../../../node_modules/atropa-inquire/docs/jsdoc/index.html">
- * ../../../node_modules/atropa-inquire/docs/jsdoc/index.html</a>,
- *  unless you installed this dependency manually.
- */
 atropa.inquire = require('atropa-inquire').inquire;
 /// <reference path="../../docs/vsdoc/OpenLayersAll.js"/>
 /*jslint
