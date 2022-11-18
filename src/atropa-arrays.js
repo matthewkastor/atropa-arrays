35c30c2bbe7f8f1e12409b85800e3e014cb7a3e6
Matthew Colter <matthewkastor@gmail.com>
2022-11-17 19:11:29 -0500

    update dev

--- src/atropa-arrays.js
+++ src/atropa-arrays.js
@@ -1,4 +1,7 @@
 /// <reference path="../docs/vsdoc/OpenLayersAll.js"/>
+/*jslint
+    node: true,
+*/
 var atropa = require('atropa-header');
 atropa.inquire = require('atropa-inquire').inquire;
 /*jslint
